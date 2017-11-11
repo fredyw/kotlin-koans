cf51ac267fc66082eb93b8416f22a9d6240e20d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-10 19:18:14 -0600

    Solve 1.4

--- src/main/kotlin/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/main/kotlin/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -1,5 +1,6 @@
 package i_introduction._4_Lambdas
 
+import com.google.common.collect.Iterables
 import util.TODO
 import util.doc4
 
@@ -22,4 +23,4 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.any {it % 2 == 0}
