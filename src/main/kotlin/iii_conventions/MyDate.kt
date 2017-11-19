6e8bb05a0e92e87d3e48f8e28af3d09ce041009b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-19 09:33:48 -0600

    Solve 3.26

--- src/main/kotlin/iii_conventions/MyDate.kt
+++ src/main/kotlin/iii_conventions/MyDate.kt
@@ -16,4 +16,6 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(d: MyDate): Boolean = start <= d && d <= endInclusive
+}
